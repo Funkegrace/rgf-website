4f2d35b7a4717066a759a1dda900bf6ab0e3d1a9
Funke Olanrewaju <olanrewajufunke28@gmail.com>
2022-04-13 15:55:16 +0100

    Update

--- src/app/@components/website/website-routing.module.ts
+++ src/app/@components/website/website-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WebsiteComponent } from './website.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 // import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
@@ -14,10 +15,10 @@ const routes: Routes = [
         path: '',
         component: HomeComponent,
       },
-      // {
-      //   path: 'about-us',
-      //   component: ,
-      // },
+      {
+        path: 'about-us',
+        component: AboutUsComponent,
+      },
     ],
   },
 
--- src/app/@components/website/website.module.ts
+++ src/app/@components/website/website.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-// import { InvestmentServicesComponent } from './about-us/investment-services/investment-services.component';
-// import { JoinUsComponent } from './about-us/join-us/join-us.component';
-// import { LandingComponent } from './about-us/landing/landing.component';
-// import { NumbersComponent } from './about-us/numbers/numbers.component';
-// import { OurMissionComponent } from './about-us/our-mission/our-mission.component';
-// import { OurStoryComponent } from './about-us/our-story/our-story.component';
-// import { OurTeamComponent } from './about-us/our-team/our-team.component';
+import { InvestmentServicesComponent } from './about-us/investment-services/investment-services.component';
+import { JoinUsComponent } from './about-us/join-us/join-us.component';
+import { LandingComponent } from './about-us/landing/landing.component';
+import { NumbersComponent } from './about-us/numbers/numbers.component';
+import { OurMissionComponent } from './about-us/our-mission/our-mission.component';
+import { OurStoryComponent } from './about-us/our-story/our-story.component';
+import { OurTeamComponent } from './about-us/our-team/our-team.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WebsiteComponent } from './website.component';
 import { WebsiteRoutingModule } from './website-routing.module';
@@ -20,24 +20,24 @@ import { LeveragingTechnologyComponent } from './home/leveraging-technology/leve
 import { WeGuaranteeComponent } from './home/we-guarantee/we-guarantee.component';
 import { CustomersFeedbackComponent } from './home/customers-feedback/customers-feedback.component';
 import { MaterialModule } from 'src/app/material/material.module';
-// import { AboutUsComponent } from './about-us/about-us.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FiguresComponent } from './home/figures/figures.component';
 import { EmailSubscriptionComponent } from './home/email-subscription/email-subscription.component';
 import { SwiperModule } from 'swiper/angular';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     HomeComponent,
     FooterComponent,
-    // LandingComponent,
-    // OurStoryComponent,
-    // OurTeamComponent,
-    // OurMissionComponent,
-    // InvestmentServicesComponent,
-    // JoinUsComponent,
-    // NumbersComponent,
+    LandingComponent,
+    OurStoryComponent,
+    OurTeamComponent,
+    OurMissionComponent,
+    InvestmentServicesComponent,
+    JoinUsComponent,
+    NumbersComponent,
     WebsiteComponent,
     HomeLandingComponent,
     LoanComponent,
@@ -48,7 +48,7 @@ import { SwiperModule } from 'swiper/angular';
     CustomersFeedbackComponent,
     FiguresComponent,
     EmailSubscriptionComponent,
-    // AboutUsComponent,
+    AboutUsComponent,
   ],
 
   imports: [
